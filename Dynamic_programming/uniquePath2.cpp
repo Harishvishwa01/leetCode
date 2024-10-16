099bbba00d6d718c4c5ae438b278d230054d27f6
Indianmask <harishvishwa12003@gmail.com>
2024-10-16 12:14:09 +0530

    leetCode 63

--- Dynamic_programming/uniquePath2.cpp
+++ Dynamic_programming/uniquePath2.cpp
@@ -3,22 +3,34 @@ using namespace std;
 
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& dp, vector<vector<int>>& grid){
-        if(i == 0 && j == 0) return 1;
-        if(i < 0 || j < 0) return 0;
-        if(grid[i][j] == 1) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int up = f(i - 1, j, dp,grid);
-        int left = f(i,j - 1,dp,grid);
-
-        return dp[i][j] = up + left;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
         int n = Grid.size();
         int m = Grid[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
 
-        return f(n-1,m-1,dp,Grid);
+        vector<int> dp(n,0);
+
+        for(int i = 0; i < n; i++){
+            vector<int> temp(m,0);
+            for(int j = 0; j < m; j++){
+                if(Grid[i][j] == 1){
+                    temp[j] = 0;
+                    continue;
+                }
+                else{
+                    if(i == 0 && j == 0 && Grid[i][j] != 1){
+                        temp[j] = 1;
+                        continue;
+                    }
+                    int up = i > 0 ? dp[j] : 0;
+                    int left = j > 0 ? temp[j -1] : 0;
+
+                    temp[j] = left + up;
+                }
+            }
+            dp = temp;
+        }
+
+        return dp[m -1];
     }
 };
 
