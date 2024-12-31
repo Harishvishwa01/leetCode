447ddeb2d52fa0c23ba128863e300201ba9e512f
Indianmask <harishvishwa12003@gmail.com>
2024-12-31 10:56:01 +0530

    547 cpp sol

---
+++ graphs/547.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void bfs(vector<int> adj[],vector<int>& vis,int start){
+        queue<int> q;
+        q.push(start);
+        vis[start] = 1;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(auto it : adj[node]){
+                if(!vis[it]){
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int v = isConnected.size();
+        vector<int> adj[v];
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                if (isConnected[i][j] && i != j) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        vector<int> vis(v,0);
+        int cnt = 0;
+        for(int i = 0; i < v; i++){
+           if(!vis[i]){
+            bfs(adj,vis,i);
+            cnt++;
+           }
+        }
+        return cnt;
+    }
+};
+
+int main(){
+
+}
